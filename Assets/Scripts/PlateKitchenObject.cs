c8c582d58dce16710e6ee1a4000f028c537c7860
Cabo1988 <lubos.cabala@gmail.com>
2024-02-16 09:53:51 +0100

    Feb 16th commit
    plate visuals added

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,8 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject {
+
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs {
+        public KitchenObjectSO kitchenObjectSO;
+    }
 
     [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
     private List<KitchenObjectSO> kitchenObjectSOList;
@@ -20,7 +26,16 @@ public class PlateKitchenObject : KitchenObject {
             return false;
         } else {
             kitchenObjectSOList.Add(kitchenObjectSO);
+
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs {
+                kitchenObjectSO = kitchenObjectSO
+            });
+
             return true;
         }
+    }
+
+    public List<KitchenObjectSO> GetKitchenObjectSOList() {
+        return kitchenObjectSOList;
     }
 }
