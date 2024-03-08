f01550e84767a0164770868fffc132e593cb0cc4
Cabo1988 <lubos.cabala@gmail.com>
2024-03-08 14:25:53 +0100

    Mar 8th commit
    Main menu and Loading scene

---
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameOverUI : MonoBehaviour {
+
+    [SerializeField] private TextMeshProUGUI recipesDepiveredText;
+
+    private void Start() {
+        KitchenGameManager.Instance.OnStateChanged += KitchenGameManager_OnStateChanged;
+
+        Hide();
+    }
+
+    private void KitchenGameManager_OnStateChanged(object sender, System.EventArgs e) {
+        if (KitchenGameManager.Instance.IsGameOver()) {
+            Show();
+
+            recipesDepiveredText.text = DeliveryManager.Instance.GetSuccessfulRecipesAmount().ToString();
+        } else {
+            Hide();
+        }
+    }
+
+    private void Show() {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide() {
+        gameObject.SetActive(false);
+    }
+}
